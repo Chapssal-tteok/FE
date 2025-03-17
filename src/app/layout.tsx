b87cebfdf031da04ff169c8418e11e9c3ba196b8
chacha091 <chacha09@ewha.ac.kr>
2025-03-17 17:54:35 +0900

    style: font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type React from "react"
 import type { Metadata } from "next";
 
-import { Inter } from "next/font/google";
+//import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 
 import "../styles/globals.css";
 import { AuthProvider } from "../contexts/AuthContext"
 
-const inter = Inter({ subsets: ["latin"] })
+//const inter = Inter({ subsets: ["latin"] })
 
 const Gmarket = localFont({
   src: [
