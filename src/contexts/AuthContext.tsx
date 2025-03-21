37495b9f942fe50893c578bc15a3ece86e842ae7
chacha091 <chacha09@ewha.ac.kr>
2025-03-21 20:47:42 +0900

    feat: user 추가

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,28 +1,50 @@
 "use client"
 
-import React, { createContext, useState, useContext, type ReactNode } from "react"
+import React, { createContext, useState, useContext, useEffect, type ReactNode } from "react"
+
+interface User{
+  id: string
+  email: string
+}
 
 interface AuthContextType {
+  user: User | null
   isLoggedIn: boolean
-  login: (token: string) => void
+  login: (token: string, user: User) => void
   logout: () => void
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined)
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"))
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [user, setUser] = useState<User | null>(null)
 
-  const login = (token: string) => {
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    const storedUser = localStorage.getItem("user")
+
+    if (token && storedUser) {
+      setIsLoggedIn(true)
+      setUser(JSON.parse(storedUser))
+    }
+  }, [])
+
+  const login = (token: string, user: User) => {
     localStorage.setItem("token", token)
+    localStorage.setItem("user", JSON.stringify(user))
+    setUser(user)
     setIsLoggedIn(true)
   }
+
   const logout = () => {
     localStorage.removeItem("token")
+    localStorage.removeItem("user")
+    setUser(null)
     setIsLoggedIn(false)
   }
 
-  return <AuthContext.Provider value={{ isLoggedIn, login, logout }}>{children}</AuthContext.Provider>
+  return <AuthContext.Provider value={{ isLoggedIn, user, login, logout }}>{children}</AuthContext.Provider>
 }
 
 export function useAuth() {
