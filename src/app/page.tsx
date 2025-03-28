cef1ad94a77f06eb6f87cc2a141b87870ec19eea
chacha091 <chacha09@ewha.ac.kr>
2025-03-29 01:24:29 +0900

    style: UI 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,7 @@ import { useState, useEffect } from "react";
 
 export default function Home() {
   const router = useRouter()
-  const { logout, isLoggedIn } = useAuth()
-  
+  const { isLoggedIn, logout } = useAuth()
   const [typedText, setTypedText] = useState("")
   const [typedSubText, setTypedSubText] = useState("")
   const [index, setIndex] = useState(0)
@@ -60,37 +59,44 @@ export default function Home() {
   }
 
   return (
-    <div className="relative min-h-screen overflow-hidden bg-gradient-to-br from-lime-50 to-white">
-      <nav className="absolute top-0 w-full p-4">
-        <div className="container flex justify-between items-center">
+    <div className="relative min-h-screen overflow-hidden">
+      <nav className="absolute top-0 w-full p-4 z-50">
+        <div className="container flex justify-between items-center mx-auto px-4">
           <Link href="/" className="text-2xl font-bold">
-            PreView
+            <div className="flex items-center">
+              <img src="/Vector.png" alt="PreView Logo" className="w-5 h-5 mb-1" />
+              <span className="ml-1">PreView</span>
+            </div>
           </Link>
 
           {isLoggedIn ? (
-            <div>
+            <div className="space-x-4">
               <Link href="/mypage">
-                <Button variant="outline">My Page</Button>
+                <Button variant="outline">
+                  My Page
+                </Button>
               </Link>
-              <Button variant="outline" onClick={handleLogout}>Log out</Button>
+              <Button variant="outline" onClick={handleLogout}>
+                Log out
+              </Button>
             </div>
           ) : (
             <Link href="/login">
-              <Button variant="outline">LogIn</Button>
-            </Link>
+              <Button variant="outline">Login</Button>
+            </Link>         
           )}
         </div>
       </nav>
 
       <main className="container flex flex-col items-center justify-center min-h-screen px-4 text-center">
-        <div className="absolute inset-0 bg-[url(/Gradients.png)] bg-center bg-cover opacity-50">
+        <div className="absolute inset-0 bg-[url(/Gradients.png)] bg-center bg-cover z-0">
           <div className="relative z-10 space-y-8 flex flex-col items-start justify-center h-full ml-80 mb-10">
             <div className="flex items-center space-x-2">
               <img src="/Vector.png" alt="icon" className="w-8 h-8 md:h-12 md:w-12 mt-[-14px]"/>
               <h1 className="text-4xl font-bold leading-tight md:text-5xl lg:text-6xl relative inline-block">
                 {typedText}
                 {index < text.length && !cursorInSubText && (
-                  <span className="absolute right-0 top-0 w-[2px] h-full bg-black animate-blink"></span>
+                  <span className="absolute w-[2px] h-full bg-black animate-blink"></span>
                 )}
               </h1>
             </div>
@@ -98,7 +104,9 @@ export default function Home() {
             <h2 className="text-2xl leading-tight text-left">
               <span dangerouslySetInnerHTML={{ __html: typedSubText.replace(/\n/g, "<br />") }} />
             </h2>
-            <Button className="text-xl px-8 py-7 rounded-3xl shadow-xl shadow-black-500/50 text-black bg-light-green hover:bg-lime-400" onClick={handleStart}>
+
+            <Button className="text-xl px-8 py-7 rounded-3xl shadow-xl shadow-black-500/50 text-black bg-light-green hover:bg-lime-400" 
+              onClick={handleStart}>
               시작하기
             </Button>
           </div>
