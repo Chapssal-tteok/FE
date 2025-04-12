ba48c5871a851c853f7543215eb34d6bb4d3c48b
chacha091 <chacha09@ewha.ac.kr>
2025-04-12 15:34:48 +0900

    feat: allowDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-
+  experimental: {
+    allowedDevOrigins: ['http://172.24.112.1:3000'],
+  },
 };
+
+module.exports = {
+  env: {
+    Pplx_API_KEY: process.env.Pplx_API_KEY,
+  },
+};
+
 
 export default nextConfig;
